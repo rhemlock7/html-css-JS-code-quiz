aba76026548fe66a4c5753db3324a72ce50c9db2
Ryan Hemlock <ryanhemlock@Ashleys-MacBook-Pro.local>
2023-11-28 06:50:32 -0500

    Created multiple choice button

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,7 +1,35 @@
+// Variables
 let timer = document.getElementById("timer");
 
+// HTML Elements
+let question = document.getElementById("h1");
+let startButton = document.getElementById("bttn");
+
+// Multiple choice container and buttons
+let paragraph = document.getElementById("p");
+
+
+function addMultipleChoice() {
+    let form = document.createElement("form");
+    let radio = document.createElement("input");
+
+    radio.type = 'button';
+    radio.value = 'value';
+    radio.className = 'button-choice'
+
+    form.appendChild(radio);
+    paragraph.replaceWith(form);
+}
+
+// Timer countdown function
 function countdown() {
+    startButton.setAttribute("style", "display:none;");
+    document.body.setAttribute("style", "text-align:left;")
+    addMultipleChoice();
     let timeLeft = 60;
+
+    question.textContent = "Testing";
+    paragraph.textContent = "Testing 2"
 
     let timeInterval = setInterval(function() {
         timeLeft--;
@@ -11,10 +39,9 @@ function countdown() {
             clearInterval(timeInterval);
             timer.textContent = "Out of time!";
             timer.setAttribute("style", "color:red")
-          }
+        }
 
     }, 1000)
-
 }
 
-// countdown()
+addMultipleChoice();
