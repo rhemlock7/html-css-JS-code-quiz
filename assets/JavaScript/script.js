6325fc26bffe4fac5de40d3fd6fbcc91fa10e72f
Ryan Hemlock <ryanhemlock@Ashleys-MacBook-Pro.local>
2023-11-28 07:52:23 -0500

    Multiple choice buttons working

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,5 +1,6 @@
 // Variables
 let timer = document.getElementById("timer");
+let timeLeft = 60; // Time allotted for timer = seconds
 
 // HTML Elements
 let question = document.getElementById("h1");
@@ -7,30 +8,46 @@ let startButton = document.getElementById("bttn");
 
 // Multiple choice container and buttons
 let paragraph = document.getElementById("p");
+let answerDiv = document.createElement("div")
+let form = document.createElement("form");
 
-
-function addMultipleChoice() {
-    let form = document.createElement("form");
+// Add dynamic multiple choice questions
+function addMultipleChoice(bttnValue) {
     let radio = document.createElement("input");
-
+    
+    //Set the input's value
     radio.type = 'button';
-    radio.value = 'value';
+    radio.value = bttnValue;
+    radio.id = bttnValue;
     radio.className = 'button-choice'
 
+    // Append the button to the form
     form.appendChild(radio);
     paragraph.replaceWith(form);
 }
 
+// Question functions
+function questionOne() {
+    question.textContent = "Commonly used data types do NOT include:";
+
+    addMultipleChoice("Strings");
+    addMultipleChoice("Booleans");
+    addMultipleChoice("Alerts");
+    addMultipleChoice("Numbers");
+
+}
+
 // Timer countdown function
 function countdown() {
+    // Remove start button and style text-align:left;
     startButton.setAttribute("style", "display:none;");
     document.body.setAttribute("style", "text-align:left;")
-    addMultipleChoice();
-    let timeLeft = 60;
 
-    question.textContent = "Testing";
-    paragraph.textContent = "Testing 2"
+    // Questions
+    questionOne();
 
+    
+    // Timer countdown. Stop quiz if timer hits 0
     let timeInterval = setInterval(function() {
         timeLeft--;
         timer.textContent = "Time: " + timeLeft;
@@ -43,5 +60,3 @@ function countdown() {
 
     }, 1000)
 }
-
-addMultipleChoice();
