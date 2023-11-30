14285a1a1580ab90a49342151133ff6022d6dfb4
Ryan Hemlock <ryan@ryanhemlock.com>
2023-11-30 09:44:33 -0500

    FIXED buttons. Buttons dynamically populate.

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,132 +1,202 @@
-// Variables
+// ---------- Variables ---------- //
+const root = $('#root');
 let timer = document.getElementById("timer");
 let timeLeft = 90; // Time allotted for timer = seconds
 
-// Questions & Options
-let questionArray = [
+// Array containing the Questions & Options
+const questions = [
     {
         question: "Commonly used data types do NOT include:",
-        options: ["Strings", "Booleans", "Alerts", "Numbers"],
-        correctAnswer: "Alerts"
+        answers: [
+            {
+                option: "Strings",
+                isCorrect: false
+            },
+            {
+                option: "Booleans",
+                isCorrect: false
+            },
+            {
+                option: "Alerts",
+                isCorrect: true
+            },
+            {
+                option: "Numbers",
+                isCorrect: false
+            }]
     },
     {
         question: "The condition of an if/else statement is enclosed with ______.",
-        options: ["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
-        correctAnswer: "Parenthesis"
+        answers: [
+            {
+                option: "Parenthesis",
+                isCorrect: true
+            },
+            {
+                option: "Curly Brackets",
+                isCorrect: false
+            },
+            {
+                option: "Quotes",
+                isCorrect: false
+            },
+            {
+                option: "Square Brackets",
+                isCorrect: false
+            }]
     },
     {
         question: "Arrays in JavaScript can be used to store ________.",
-        options: ["Numbers & Strings", "Other Arrays", "Booleans", "All of the above"],
-        correctAnswer: "All of the above"
+        answers: [
+            {
+                option: "Numbers & Strings",
+                isCorrect: false
+            },
+            {
+                option: "Other Arrays",
+                isCorrect: false
+            },
+            {
+                option: "Booleans",
+                isCorrect: false
+            },
+            {
+                option: "All of the above",
+                isCorrect: true
+            }]
     },
     {
         question: "String values must be enclosed within ______ when being assigned to variables.",
-        options: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
-        correctAnswer: "Quotes"
+        answers: [
+            {
+                option: "Commas",
+                isCorrect: false
+            },
+            {
+                option: "Quotes",
+                isCorrect: true
+            },
+            {
+                option: "Curly Brackets",
+                isCorrect: false
+            },
+            {
+                option: "Parenthesis",
+                isCorrect: false
+            }]
     },
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        options: ["JavaScript", "Terminal/Bash", "For Loops", "Console Log"],
-        correctAnswer: "Console Log"
+        answers: [
+            {
+                option: "JavaScript",
+                isCorrect: false
+            },
+            {
+                option: "Terminal/Bash",
+                isCorrect: false
+            },
+            {
+                option: "For Loops",
+                isCorrect: false
+            },
+            {
+                option: "Console Log",
+                isCorrect: true
+            }]
     }
-];
-    
+]
 
-// HTML Elements
-let question = document.getElementById("h1");
+// ---------- HTML Elements ---------- //
+let questionh1 = document.getElementById("h1");
 let startButton = document.getElementById("bttn");
-let answerDiv = document.createElement("div")
-let answerText = document.createElement("p")
+let paragraph = document.getElementById("p")
 
-// Multiple choice container and buttons
-let paragraph = document.getElementById("p");
-let form = document.createElement("form");
-let questionCount;
-questionCount = 0;
+// ---------- Question & Score tracking ---------- //
+let currentQuestionIndex = 0;
+let score = 0;
 
-// Add dynamic multiple choice questions
-function addMultipleChoice(bttnValue) {
+
+// ---------- Create question ---------- //
+function displayQuestion(questionArray){
+    questionh1.textContent = questionArray.question;
+
+    const multipleChoiceContainer = document.createElement('div');
     
-    let radio = document.createElement("input");
+    // Add the multiple choice answers
+    questionArray.answers.forEach(answer => {
+        // console.log(answer.option);
+        // create multiple choice container div
+        // create the buttons to be within the div
+        const choiceButton = document.createElement('button');
+        // $("<button>")
 
-    //Set the input's value
-    radio.type = 'button';
-    radio.value = bttnValue;
-    radio.id = bttnValue;
-    radio.className = 'button-choice'
+        // Button value === index. Passing index through displayQuestion function
+        choiceButton.dataset.answer = answer.isCorrect;
+        choiceButton.textContent = answer.option;
 
-    // Append the button to the form
-    form.appendChild(radio);
-    paragraph.replaceWith(form);
+        // Append the button to the container
+        multipleChoiceContainer.append(choiceButton);
+        paragraph.replaceWith(multipleChoiceContainer);
 
-    // Check correct answer
-    radio.addEventListener("click", function(indexNumber){
-        let buttonValue = bttnValue;
-        // console.log(buttonValue)
-        if (buttonValue == questionArray[indexNumber].correctAnswer) {
-            console.log("CORRECT")
-            form.appendChild(answerDiv);
-            answerText.textContent = "✅ Correct!"
-            questionCount++;
-            displayQuestion(questionCount)
-        } else {
-            form.appendChild(answerDiv);
-            answerText.textContent = "❌ Wrong answer... Deducting 10 seconds."
-            timeLeft = timeLeft - 10;
-            questionCount++;
-            displayQuestion(questionCount)
-        }
-    })
+        // Event listener to handle click
+        choiceButton.addEventListener('click', selectAnswer);
+    });
 }
 
-
-// Question functions
-function displayQuestion(indexNumber) {
-    question.textContent = questionArray[indexNumber].question;
-
-    if (indexNumber === 0) {
-        addMultipleChoice(questionArray[indexNumber].options[0]);
-        addMultipleChoice(questionArray[indexNumber].options[1]);
-        addMultipleChoice(questionArray[indexNumber].options[2]);
-        addMultipleChoice(questionArray[indexNumber].options[3]);
+// ---------- Determines if answer is correct ---------- //
+function selectAnswer(event) {
+    const selectButton = event.target;
+    // console.log(selectButton)
+    if (selectButton.dataset.answer === "true") {
+        console.log("true")
+        const answerDisplay = document.createElement('div')
+        const answerText = document.createElement('p');
+        // setTimeout(
+        //     answerText.textContent = "✅ Correct!"
+        //     answerDisplay.append(answerText)
+        //     paragraph.append(answerDisplay)
+        // ,1000)
+        // say "✅ Correct"
+        
+        // increase "currentQuestionIndex" to go to next question
     } else {
-        $("#Strings").val(questionArray[indexNumber].options[0])
-        $("#Booleans").val(questionArray[indexNumber].options[1])
-        $("#Alerts").val(questionArray[indexNumber].options[2])
-        $("#Numbers").val(questionArray[indexNumber].options[3])
+        console.log("false")
+        // say "❌ Wrong answer... deducting 10 seconds."
+        // increase "currentQuestionIndex" to go to next question
     }
+}
 
-    // Append answer reveal
-    answerDiv.id = 'answer-div'
-    answerDiv.appendChild(answerText);
-
-} 
-
-
-// Timer countdown function
-function startQuiz(indexNumber) {
+// ---------- Timer countdown function ---------- //
+function startQuiz() {
     // Remove start button and style text-align:left;
     startButton.setAttribute("style", "display:none;");
     document.body.setAttribute("style", "text-align:left;")
 
-    // Questions - Start at the first question "0"
-    displayQuestion(indexNumber);
+    currentQuestionIndex = 0;
+    score = 0;
+    displayQuestion(questions[currentQuestionIndex]);
     
     // Timer countdown. Stop quiz if timer hits 0
-    let timeInterval = setInterval(function() {
-        timeLeft--;
-        timer.textContent = "Time: " + timeLeft;
+    // let timeInterval = setInterval(function() {
+    //     timeLeft--;
+    //     timer.textContent = "Time: " + timeLeft;
 
-        if(timeLeft === -1) {
-            clearInterval(timeInterval);
-            timer.textContent = "Out of time!";
-            timer.setAttribute("style", "color:red")
-            question.textContent = "Quiz Ended 😔";
-            answerText.textContent = "";
-            form.textContent = "";
-        }
+    //     if(timeLeft === -1) {
+    //         clearInterval(timeInterval);
+    //         timer.textContent = "Out of time!";
+    //         timer.setAttribute("style", "color:red")
+    //         question.textContent = "Quiz Ended 😔";
+    //         answerText.textContent = "";
+    //         form.textContent = "";
+    //     }
 
-    }, 1000)
+    // }, 1000)
 }
 
-startQuiz(questionCount);
+// ---------- End of Quiz - Submit Score ---------- //
+
+
+// ---------- Show High Scores ---------- //
+
+startQuiz()
